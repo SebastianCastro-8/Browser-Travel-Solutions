122ae2b8fde14903d952454774fae5164980a305
Sebastián Castro <sebastian.castro@monolegal.co>
2023-03-21 16:07:20 -0500

    *Estructura para tests

---
+++ TheLibraryTravel/src/TheLibraryTravel.WebApi.Tests/LibroControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheLibraryTravel.WebApi.Tests
+{
+    public class LibroControllerTests
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
